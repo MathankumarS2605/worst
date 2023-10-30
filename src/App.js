a3c2471d0823ed2e3af766cabd9bde9785208da4
mathankumar.s <mathankumar.s@200411LTP2851>
2023-10-30 10:28:37 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import {useState} from 'react';
+function App(props) {
+  var username=props.username;
+   var time=new Date();
+   console.log(time.getHours() + " Greater than " +   props.visitingTime);
+  if(props.name == username && props.visitingTime >= time.getHours()){ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <b>Welcome to MS </b>{props.firstName} &nbsp;{props.lastname}
     </div>
   );
+  }
+  else{
+    return( <center><div><b> Please contact administartor.  </b>
+    </div></center>
+    );
+   
+  }
 }
 
 export default App;
